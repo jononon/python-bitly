4000a8fb7ad5f411e0e51bde9d203fc81078013b
yoav.aviram <yoav.aviram@7bfdbab4-5415-11de-907f-3386865dedc4>
2009-08-14 14:45:36 +0000

    Modify API class, add (object)

--- trunk/bitly.py
+++ trunk/bitly.py
@@ -38,7 +38,7 @@ class BitlyError(Exception):
     '''Returns the first argument used to construct this error.'''
     return self.args[0]
 
-class Api():
+class Api(object):
     """ API class for bit.ly """
     def __init__(self, login, apikey):
         self.login = login
